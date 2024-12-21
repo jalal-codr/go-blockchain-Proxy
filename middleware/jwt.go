c262d0922bd163d19f44aa4f85109061b02a13f9
jalal-codr <abduljalal849@gmail.com>
2024-12-21 22:11:18 +0100

    Api update

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,12 +1,16 @@
 package middleware
 
 import (
+	"errors"
+	"go/token"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
 var jwtSecret = []byte("sdfghjkhgvfjbhgfdfdsdfghjwertyu")
+
+var jwtError = errors.New("Invalid token.")
 
 func GenerateToken(publicKey string) (string, error) {
 	claims := jwt.MapClaims{
@@ -15,4 +19,17 @@ func GenerateToken(publicKey string) (string, error) {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
 	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwtError
+		}
+		return jwtSecret, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, err
 }
